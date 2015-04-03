AKSW/rocker

commit de64a5bcc2346fd634e0c41f10e5263271f826f8

save score for almost-keys

rocker-parent/src/main/java/de/uni_leipzig/simba/keydiscovery/rockerone/Algorithm.java
@@ -147,7 +147,7 @@ public class Algorithm {
 			Set<CandidateNode> children = refine(pivot, atomic, c, keys, maxNonKeys, atomicMap);
 			updateNonKeys(maxNonKeys, children);
 			for(CandidateNode child : children) {
-				if(child.getScore() == 1.0) {
+				if(child.isAlmostKey()) {
 					keys.add(child);
 					print("Key: "+child);
 					if(FIND_ONE_KEY)
@@ -155,7 +155,7 @@ public class Algorithm {
 				}
 			}
 			for(CandidateNode child : children) {
-				if(child.getScore() == 1.0) {
+				if(child.isAlmostKey()) {
 					if(FAST_SEARCH) {
 						Iterator<CandidateNode> it = atomic.iterator();
 						while(it.hasNext()) {

rocker-parent/src/main/java/de/uni_leipzig/simba/keydiscovery/rockerone/Score.java
@@ -80,12 +80,11 @@ public class Score {
 			e.printStackTrace();
 		}
 		
-		// TODO better implement this hack for getting quasi-keys
-//		return (double) distincts.size() / c.getResourceCount();
 		if(distincts.size() >= quasi_keys_rate * c.getResourceCount()) {
 			double d = (double) distincts.size() / c.getResourceCount();
 			LOGGER.info("almost-key found: "+d+" "+cn.getProperties());
-			return 1.0;
+			cn.setAlmostKey(true);
+			return d;
 		}
 		return (double) distincts.size() / c.getResourceCount();
 	}

rocker-parent/src/main/java/de/uni_leipzig/simba/keydiscovery/model/CandidateNode.java
@@ -24,6 +24,7 @@ public class CandidateNode implements Comparable<CandidateNode> {
 	private List<CandidateNode> children;
 	// not used
 	private int index;
+	private boolean isAlmostKey = false;
 	
 	private Set<String> faultyResourceURIs;
 
@@ -48,6 +49,14 @@ public class CandidateNode implements Comparable<CandidateNode> {
 
 	public double getScore() {
 		return score;
+	}
+
+	public boolean isAlmostKey() {
+		return isAlmostKey;
+	}
+
+	public void setAlmostKey(boolean isAlmostKey) {
+		this.isAlmostKey = isAlmostKey;
 	}
 
 	public int getIndex() {

commit 5845385edc0c66fdc677341d05e047d4db90f826

fixed bug introduced by de64a5b

rocker-parent/pom.xml
@@ -4,7 +4,7 @@
 
 	<groupId>org.aksw.rocker</groupId>
 	<artifactId>rocker</artifactId>
-	<version>1.2.2</version>
+	<version>1.3</version>
 	<packaging>jar</packaging>
 
 	<name>rocker-parent</name>

rocker-parent/src/main/java/de/uni_leipzig/simba/keydiscovery/model/Issue.java
@@ -33,8 +33,8 @@ public class Issue {
 		for(Property p : map.keySet()) {
 			Resource s = m.getResource(fres);
 			Set<RDFNode> objset = m.listObjectsOfProperty(s, p).toSet();
-			LOGGER.info("<"+s+"> <"+p+">");
-			LOGGER.info("size(objset) = "+objset.size());
+			LOGGER.debug("<"+s+"> <"+p+">");
+			LOGGER.debug("size(objset) = "+objset.size());
 			map.put(p, objset);
 			
 			// TODO
@@ -42,7 +42,7 @@ public class Issue {
 				;
 		}
 		
-		LOGGER.info(map);
+		LOGGER.debug(map);
 		
 	}
 

rocker-parent/src/main/java/de/uni_leipzig/simba/keydiscovery/rockerone/Algorithm.java
@@ -100,11 +100,12 @@ public class Algorithm {
 			Set<Property> pSet = new HashSet<Property>();
 			pSet.add(p);
 			CandidateNode cn = new CandidateNode(pSet);
+			atomicCandidates.add(cn);
 			double score = scr.getScore(c, cn);
 			cn.setScore(score);
 			print("Candidate: "+score+"\t|P|="+pSet.size()+"\t"+cn);
 			overallVisitedNodes++;
-			if(score == 1.0) {
+			if(cn.isAlmostKey()) {
 				keys.add(cn);
 				print("Key: "+cn);
 				if(FIND_ONE_KEY)
@@ -118,7 +119,6 @@ public class Algorithm {
 		while(itr.hasNext()) {
 			CandidateNode cand = itr.next();
 			print("score("+cand+") = "+cand.getScore());
-			atomicCandidates.add(cand);
 			if(cand.getScore() < 1E-3 && PROP_REDUCTION)
 				itr.remove();
 		}
@@ -318,13 +318,13 @@ public class Algorithm {
 		print("Top element "+cn+" has score = "+score);
 		if(topElement == null)
 			topElement = cn;
-		return -1.0 < score && score < 1.0;
+		return !cn.isAlmostKey();
 	}
 
 	public CandidateNode getAtomicCandidate(Property p) {
 		for(CandidateNode cn : atomicCandidates) {
 			Property p1 = cn.getProperties().iterator().next();
-			if(p == p1)
+			if(p.getURI().equals(p1.getURI()))
 				return cn;
 		}
 		return null;

rocker-parent/src/main/java/de/uni_leipzig/simba/keydiscovery/rockerone/Rocker.java
@@ -51,7 +51,7 @@ public class Rocker implements Runnable {
 		this.find_one_key = find_one_key;
 		this.fast_search = fast_search;
 		this.sql = new SQLiteManager(WORKSPACE_DIR + dataset);
-		LOGGER.info(WORKSPACE_DIR + dataset);
+		LOGGER.info("name: " + WORKSPACE_DIR + dataset);
 		this.scr = new Score(dataset, sql, quasi_keys_rate);
 	}
 

rocker-parent/src/main/java/de/uni_leipzig/simba/keydiscovery/rockerone/Score.java
@@ -62,7 +62,7 @@ public class Score {
 				String resourceID = hashes[0];
 				for(int i=1; i<hashes.length; i++) {
 					String hash = hashes[i];
-					// code for Atencia's supposedly wrong definition of keys
+					// code for Atencia's incomplete definition of keys
 					if(!ROCKER_KEY_DEFINITION)
 						if(hash.equals("null"))
 							continue outer;